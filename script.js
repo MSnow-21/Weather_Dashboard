788dc24878fdb5912b78107e0b8b1aceef479a8c
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-01-13 07:19:40 -0500

    Added link to moment script. Created html elements and text in jquery call

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // API Testing
 
-
 $("#search-button").on("click", function(event){
     event.preventDefault();
 
@@ -33,6 +32,17 @@ function searchWeather(cityName){
       var humidity = response.main.humidity
       var windSpeed = response.wind.speed;
 
+
+      var nameH1 = $("<h1>")
+      nameH1.text(name)
+      var tempH3 = $("<h3>")
+      tempH3.text("Temperature: " + temp + " degrees F")
+      var humidityH3 = $("<h3>")
+      humidityH3.text("Humidity: " + humidity +" %")
+      var windspeedH3 = $("<h3>");
+      windspeedH3.text("Windspeed: " + windSpeed + " mph")
+      displayToday.empty();
+      displayToday.append(nameH1,tempH3,humidityH3,windspeedH3);
   
     });
 };
