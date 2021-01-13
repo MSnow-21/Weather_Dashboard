9fcbc9aa32c9db4118737fde699c18e474b8ae09
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-01-13 08:28:09 -0500

    Created new call to obtain uv information. Added html to current day div to add uv info. Added description notes to script

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // API Testing
 
-
 $("#search-button").on("click", function(event){
     event.preventDefault();
 
@@ -33,6 +32,12 @@ function searchWeather(cityName){
       var humidity = response.main.humidity
       var windSpeed = response.wind.speed;
 
+      // assigning longitude and latitude for function below
+
+      latitude = response.coord.lat
+
+      longitude = response.coord.lon
+
       // Date
       var m = moment().format("MM"+"/"+"D"+"/"+"YY")
 
@@ -47,16 +52,45 @@ function searchWeather(cityName){
       humidityH3.text("Humidity: " + humidity +" %")
       var windspeedH3 = $("<h3>");
       windspeedH3.text("Windspeed: " + windSpeed + " mph")
+      // adding attribute to windspeedH3 to see if other call can use it
+      windspeedH3.attr("class", "windDayOne");
       //adding moment variable html
       var timeM = $("<h7>");
       timeM.text("("+m+")");
       //creating image variable html
       var imageOne = $("<img>");
       imageOne.attr("src", "http://openweathermap.org/img/wn/"+iconImage+"@2x.png")
-      displayToday.empty();
+      displayToday.empty(); //empties contents of div before new information is added
       displayToday.append(nameH1,tempH3,humidityH3,windspeedH3);
       nameH1.append(timeM);
       timeM.append(imageOne);
+
+
+      // adding another query call to get the UV index information. need longitude and latitude from above
+
+
+      var uvindexURL = "https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&exclude=hourly&units=imperial&appid=" + APIKey;
+    
+      $.ajax({
+      url: uvindexURL,
+      method: "GET"
+      }).then(function(response){
+      console.log(response);
+
+
+      uvToday = response.current.uvi
+      console.log(uvToday);
+      //localStorage.setItem("uvItem", uvItem);
+      windFirstDay = $(".windDayOne");
+      uvTodayH5 = $("<h5>");
+      uvTodayH5.text("UV Rating: "+uvToday);
+      windFirstDay.append(uvTodayH5);
+
+      
+
+    });
+
+
       
     });
 };
